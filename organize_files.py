f8e689e43e880a54143a70f28fcef7f37f9c986c
Isaac Shure <shureisaac@gmail.com>
2025-05-21 11:43:42 -0700

    Use dedicated input directories

--- organize_files.py
+++ organize_files.py
@@ -23,8 +23,8 @@ def _(os):
 
 @app.cell
 def _(os):
-    def get_pdfs(newest_first=True):
-        files = os.listdir()
+    def get_pdfs(input_directory, newest_first=True):
+        files = os.listdir(input_directory)
         pdf_files = [f for f in files if f.endswith(".pdf")]
         return sorted(pdf_files, key=os.path.getmtime, reverse=newest_first)
     return (get_pdfs,)
@@ -37,14 +37,14 @@ def _(pdf2image, pypdf, pytesseract):
         images = pdf2image.convert_from_path(filename)
 
         pages = []
-    
+
         for page, image in zip(reader.pages, images):
             osd = pytesseract.image_to_osd(image, output_type=pytesseract.Output.DICT)
             rotation = int(osd['rotate'])
-        
+
             if rotation != 0:
                 page.rotate(rotation)
-            
+
             pages.append(page)
 
         return pages
@@ -53,15 +53,15 @@ def _(pdf2image, pypdf, pytesseract):
 
 @app.cell
 def _(autorotate_pdf, get_pdfs, itertools, pypdf):
-    def __main__(output_filename, newest_first=True, autorotate=True):
+    def __main__(input_directory, output_filename, newest_first=True, autorotate=True):
         writer = pypdf.PdfWriter()
-        pdf_filenames = get_pdfs(newest_first=newest_first)
+        pdf_filenames = get_pdfs(input_directory, newest_first=newest_first)
 
         if autorotate is True:
             pages = itertools.chain.from_iterable([autorotate_pdf(filename) for filename in pdf_filenames])
         else:
             pages = itertools.chain.from_iterable([pypdf.PdfReader(filename).pages for filename in pdf_filenames])
-    
+
         for page in pages:
             writer.add_page(page)
 
@@ -78,23 +78,23 @@ def _(Sequence, pypdf):
         assert len(page_numbers) == len(rotations)
 
         pages = pypdf.PdfReader(filename).pages
-    
+
         for page_number, rotation in zip(page_numbers, rotations):
             pages[page_number].rotate(rotation)
-    
+
         writer = pypdf.PdfWriter()
 
         for page in pages:
             writer.add_page(page)
-        
+
         with open(filename, "wb") as f:
             writer.write(f)
-    return (rotate_pages,)
+    return
 
 
 @app.cell
-def _(rotate_pages):
-    rotate_pages("H.pdf",[])
+def _():
+    #rotate_pages("H.pdf",[])
     return
 
 
@@ -116,12 +116,10 @@ def _(get_pdfs):
     return
 
 
-app._unparsable_cell(
-    r"""
-    |[(file, os.path.getmtime(file)) for file in os.listdir()]
-    """,
-    name="_"
-)
+@app.cell
+def _(os):
+    [(file, os.path.getmtime(file)) for file in os.listdir()]
+    return
 
 
 @app.cell
